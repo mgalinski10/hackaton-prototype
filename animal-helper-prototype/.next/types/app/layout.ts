d02dd8d54080cd4e9f21d3beff9dcc2cacfd36b8
mkalejta <mikolajkalejta@gmail.com>
2026-03-08 08:32:20 +0100

    nowe zmiany

--- animal-helper-prototype/.next/types/app/layout.ts
+++ animal-helper-prototype/.next/types/app/layout.ts
@@ -1,4 +1,4 @@
-// File: /home/michal/Hackaton/hackaton-prototype/animal-helper-prototype/src/app/layout.tsx
+// File: /Users/mikolajkalejta/hackaton-prototype/animal-helper-prototype/src/app/layout.tsx
 import * as entry from '../../../src/app/layout.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
